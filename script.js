0f4ceeeb0a8b52343e9fdf3feb88facd78a9b1ec
Allick <135267@Haleys-MacBook-Air.local>
2021-03-30 00:40:47 -0500

    added JSON

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
   const launchStatus = document.getElementById("launchStatus");
   const faultyItems=document.getElementById("faultyItems");
   const fuelStatus = document.getElementById("fuelStatus");
-  
   let button= document.getElementById("formSubmit");
   button.addEventListener("click", function (event) { 
       //alert("submit clicked");
@@ -36,63 +35,49 @@
       //console.log(input3.value);
     //launchStatus.addEventListener("click", function() {
     if (fuelLevel.value < 10000) {
+      faultyItems.style.visibility = "visible";
       document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
       document.getElementById("launchStatus").style.color = "red";
-      faultyItems.style.visibility = "visible";
       document.getElementById("fuelStatus").innerHTML = "There isn't enough fuel for the journey.";
     }
     if (cargoMass.value > 10000) {
        faultyItems.style.visibility = "visible";
        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
-      document.getElementById("launchStatus").style.color = "red";
+       document.getElementById("launchStatus").style.color = "red";
        document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off.";
-  }
-})
+    }
+     else
+    {
+     document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
+     document.getElementById("launchStatus").style.color = "green";
+    }
+  })
   event.preventDefault();
-    })
-  }
-
-
-  
-  window.addEventListener("load", init);
-    
-     //aultyItems.addEventListener("click", function(event) { 
-      
+   })
    
-    
-     
-     
+}
+
+
+  window.addEventListener("load", init);
+
+  window.addEventListener("load", function(event) {
+    let json=[];
+    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then( function(json) {
+        const div= document.getElementById("missionTarget");
+        missionTarget.innerHTML = `
+                  <ol>
+                  <li>Name: ${json[5].name}</li>
+                  <li>Diameter: ${json[5].diameter}</li>
+                  <li>Star: ${json[5].star}</li>
+                  <li>Distance from Earth: ${json[5].distance}</li>
+                  <li>Number of Moons: ${json[5].moons}</li>
+                  </ol> 
+                  <img src="${json[5].image}">
+                   `;
+      })
       
-    
-  
-  
- 
-    
+                  });
+                });
 
-
-    
-  
-  
-  //     
-  //     // let input3=document.getElementById("launchStatus");
-  //     // console.log(input3.value);
-      
-  //   })
-  // }
-  
-
-    
-  
-
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+                
