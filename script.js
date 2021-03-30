ae4ab186293d184a384e8c4bb072630fd78ee8df
Allick <135267@Haleys-MacBook-Air.local>
2021-03-30 00:38:52 -0500

    completed JSON

--- script.js
+++ script.js
@@ -32,17 +32,27 @@
     //function update() {
     let button= document.getElementById("formSubmit");
     button.addEventListener("click", function (event) { 
+    let input3 = document.getElementById("launchStatus");
+      //console.log(input3.value);
     //launchStatus.addEventListener("click", function() {
-    
     if (fuelLevel.value < 10000) {
+      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
       document.getElementById("launchStatus").style.color = "red";
       faultyItems.style.visibility = "visible";
-      //alert("Fuel Level is too low!");
+      document.getElementById("fuelStatus").innerHTML = "There isn't enough fuel for the journey.";
     }
-  })
+    if (cargoMass.value > 10000) {
+       faultyItems.style.visibility = "visible";
+       document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+      document.getElementById("launchStatus").style.color = "red";
+       document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off.";
+  }
+})
   event.preventDefault();
     })
   }
+
+
   
   window.addEventListener("load", init);
     
