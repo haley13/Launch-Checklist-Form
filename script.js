a6cdf4f1ef786c6c02c74ebff52f3c5e94e5fe88
Allick <135267@Haleys-MacBook-Air.local>
2021-03-31 18:44:30 -0500

    finished

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Write your JavaScript code here!
-  function init () {
+function init () {
   //const pilotName = document.getElementById("pilotName");
   const launchStatus = document.getElementById("launchStatus");
   const faultyItems=document.getElementById("faultyItems");
   const fuelStatus = document.getElementById("fuelStatus");
-  let button= document.getElementById("formSubmit");
-  button.addEventListener("click", function (event) { 
-      //alert("submit clicked");
-      
+    let button= document.getElementById("formSubmit");
+    button.addEventListener("click", function (event) { 
+        //alert("submit clicked")
       
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
@@ -15,51 +14,46 @@
       let cargoMass = document.querySelector("input[name=cargoMass]");
       
       let input = document.getElementById("pilotStatus");
-              input.value = input.value +  `${pilotName}`;
-              console.log(input.value);
+      pilotStatus.value = pilotStatus.value +  `${pilotName}`;
+      console.log(input.value);
       let input2 = document.getElementById("copilotStatus");
-              input2.value = input2.value +  `${copilotName}`;
-              console.log(input2.value);
+      input2.value = input2.value +  `${copilotName}`;
+      console.log(input2.value);
               
-    if (pilotName.value === "" || copilotName.value === "" || fuelLevel==="" || cargoMass==="") {
-      alert("All fields are required!");
-    }
-    if (isNaN(fuelLevel.value) || isNaN(cargoMass.value )) {
-       alert("Fuel Level and Cargo Mass must be a number!"); 
-    }
-  
-    //function update() {
-    let button= document.getElementById("formSubmit");
-    button.addEventListener("click", function (event) { 
-    let input3 = document.getElementById("launchStatus");
-      //console.log(input3.value);
-    //launchStatus.addEventListener("click", function() {
-    if (fuelLevel.value < 10000) {
-      faultyItems.style.visibility = "visible";
-      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
-      document.getElementById("launchStatus").style.color = "red";
-      document.getElementById("fuelStatus").innerHTML = "There isn't enough fuel for the journey.";
-    }
-    if (cargoMass.value > 10000) {
-       faultyItems.style.visibility = "visible";
-       document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
-       document.getElementById("launchStatus").style.color = "red";
-       document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off.";
-    }
-     else
-    {
-     document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch.";
-     document.getElementById("launchStatus").style.color = "green";
-    }
+       if (pilotName.value === "" || copilotName.value === "" || fuelLevel==="" || cargoMass==="") {
+         alert("All fields are required!");
+       }
+         if (isNaN(fuelLevel.value) || isNaN(cargoMass.value )) {
+          alert("Fuel Level and Cargo Mass must be a number!"); 
+         }
+        
+        //let launchStatus= document.getElementById("launchStatus");
+        //launchStatus.addEventListener("click", function(event) {
+        //let input3 = document.getElementById("launchStatus");
+        //console.log(input3.value);
+        else if  (fuelLevel.value < 10000) {
+        faultyItems.style.visibility = "visible";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.style.color = "red";
+        fuelStatus.innerHTML = "Fuel level too low for launch";
+        }
+        else if (cargoMass.value > 10000) {
+        faultyItems.style.visibility = "visible";
+        cargoStatus.innerHTML = "There is too much mass for the shuttle to take off.";
+         }
+        else
+        {
+        launchStatus.innerHTML = "Shuttle is ready for launch.";
+        launchStatus.style.color = "green";
+        }
+        event.preventDefault();
   })
-  event.preventDefault();
-   })
-   
 }
-
-
+    
+  
   window.addEventListener("load", init);
-
+  
+  
   window.addEventListener("load", function(event) {
     let json=[];
     fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
@@ -77,7 +71,5 @@
                    `;
       })
       
-                  });
-                });
-
-                
+    });
+})
